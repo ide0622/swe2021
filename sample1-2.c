3e56825847574cd67af0665e725763b4b991cc8e
idecchi <s1911454@s.tsukuba.ac.jp>
2021-10-16 13:49:24 +0900

    add 4 C files

---
+++ sample1-2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int i =10;
+    float j =10;
+    char k ='a';
+    i = i%3; 
+    j = j/3;
+    k = k +=1;
+    printf("i = %d \n", i);
+    printf("j = %f \n", j);
+    printf("k = %c \n", k);
+
+}
